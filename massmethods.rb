758d16f88ec87429da3a46d0788f490813c14c32
xchall <macsnebyvalov@yandex.ru>
2024-09-09 09:23:35 +0300

    методы для обработки массива

---
+++ massmethods.rb
@@ -0,0 +1,17 @@
+#минимальный элемент
+def min_item(arr)
+	min = arr[0]
+	for x in arr do
+		min = x if x < min
+	end
+end	
+#первый положительный элемент
+def num_first_pos(arr)
+	i = 0
+	while i < arr.length do 
+		return i if arr[i] > 0
+		i+=1
+	end
+	#если не нашлось ни одного положительного элемента -1
+	return -1
+end
