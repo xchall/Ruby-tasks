91ba2a8a1da7016b781bbde86e94a087e349f87a
xchall <macsnebyvalov@yandex.ru>
2024-10-04 20:49:05 +0300

    переопределил метод to_s и добавил забытое id

--- Students/student.rb
+++ Students/student.rb
@@ -1,5 +1,6 @@
 class Student
 	def initialize(surname, name, patronymic, id = nil, phone = nil, teleg = nil, email = nil, git = nil)
+		@id = id
 		@surname = surname
 		@name = name 
 		@patronymic = patronymic
@@ -9,13 +10,16 @@ class Student
 		@git = git
 	end
 	#геттеры
+	def id
+		@id
+	end
 	def surname
 		@surname
 	end
 	def name
 		@name
 	end
-	def patronimic
+	def patronymic
 		@patronymic
 	end
 	def phone
@@ -31,14 +35,17 @@ class Student
 		@git
 	end
 	#сеттеры
+	def id=(id_v)
+		@id = id_v
+	end
 	def surname=(surname_v)
 		@surname = surname_v
 	end
 	def name=(name_v)
 		@name = name_v
 	end
-	def patronimic=(patronimic_v)
-		@patronymic = patronimic_v
+	def patronymic=(patronymic_v)
+		@patronymic = patronymic_v
 	end
 	def phone=(phone_v)
 		@phone = phone_v
@@ -51,6 +58,9 @@ class Student
 	end
 	def git=(git_v)
 		@git = git_v
+	end
+	def to_s
+		"id = #{@id || 'не задано'},\n surname = #{@surname},\n name = #{@name},\n patronymic = #{@patronymic},\n phone = #{@phone || 'не задано'},\n teleg = #{@teleg || 'не задано'},\n email = #{@email || 'не задано'},\n git = #{@git || 'не задано'}"
 	end
 
 end
