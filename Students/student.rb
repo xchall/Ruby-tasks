b5bbf74ac7639f83836501547da09b2e1d6f22ac
xchall <macsnebyvalov@yandex.ru>
2024-10-05 08:19:57 +0300

    Использовал атрибуты для кратк опис get/set

--- Students/student.rb
+++ Students/student.rb
@@ -1,4 +1,5 @@
 class Student
+	attr_accessor :surname, :name, :patronymic, :id, :phone, :teleg, :email, :git
 	def initialize(surname, name, patronymic, id = nil, phone = nil, teleg = nil, email = nil, git = nil)
 		@id = id
 		@surname = surname
@@ -8,56 +9,6 @@ class Student
 		@teleg = teleg
 		@email = email
 		@git = git
-	end
-	#геттеры
-	def id
-		@id
-	end
-	def surname
-		@surname
-	end
-	def name
-		@name
-	end
-	def patronymic
-		@patronymic
-	end
-	def phone
-		@phone
-	end
-	def teleg
-		@teleg
-	end
-	def email
-		@email
-	end
-	def git
-		@git
-	end
-	#сеттеры
-	def id=(id_v)
-		@id = id_v
-	end
-	def surname=(surname_v)
-		@surname = surname_v
-	end
-	def name=(name_v)
-		@name = name_v
-	end
-	def patronymic=(patronymic_v)
-		@patronymic = patronymic_v
-	end
-	def phone=(phone_v)
-		@phone = phone_v
-	end
-	def teleg=(teleg_v)
-		@teleg = teleg_v
-	end
-	def email=(email_v)
-		@email = email_v
-	end
-	def git=(git_v)
-		@git = git_v
 	end
 	def to_s
 		"id = #{@id || 'не задано'},\n surname = #{@surname},\n name = #{@name},\n patronymic = #{@patronymic},\n phone = #{@phone || 'не задано'},\n teleg = #{@teleg || 'не задано'},\n email = #{@email || 'не задано'},\n git = #{@git || 'не задано'}"
