3800fed1bba75a891e63ae75d452c82b1d1cc5f7
xchall <macsnebyvalov@yandex.ru>
2024-10-05 10:25:32 +0300

    Добавил валидацию для phone

--- Students/student.rb
+++ Students/student.rb
@@ -5,10 +5,21 @@ class Student
 		@surname = surname
 		@name = name 
 		@patronymic = patronymic
-		@phone = phone
+		if Student.phone_valid?(phone) == true
+			@phone = phone
+		else
+			raise ArgumentError, "Коля гей #{phone}"
+		end
 		@teleg = teleg
 		@email = email
 		@git = git
+	end
+	def self.phone_valid?(phone)
+		if phone == nil
+			false
+		else
+			phone.match?(/^\+7\d{10}$/)
+		end
 	end
 	def to_s
 		"id = #{@id || 'не задано'},\n surname = #{@surname},\n name = #{@name},\n patronymic = #{@patronymic},\n phone = #{@phone || 'не задано'},\n teleg = #{@teleg || 'не задано'},\n email = #{@email || 'не задано'},\n git = #{@git || 'не задано'}"
