623cbcdd8edaf5965549744a75787a386f4fe7fc
xchall <macsnebyvalov@yandex.ru>
2024-10-05 08:40:20 +0300

    Добавление значений по ключу

--- Students/main.rb
+++ Students/main.rb
@@ -1,9 +1,9 @@
 require_relative "student"
 students = [
-	Student.new("Таран", "Дмитрий", "Сергеевич",),
-	Student.new("Бачурин", "Данил", "Петрович", "451"),
-	Student.new("Прокопенко", "Юрий", "Данилович"),
-	Student.new("Зубенко", "Михаил", "Петрович", "32", "+79528502711"),
+	Student.new(surname:"Таран", name:"Дмитрий", patronymic:"Сергеевич", email:"dimamolod@gmail.com"),
+	Student.new(surname:"Бачурин", name:"Данил", patronymic:"Петрович", id:"451", git:"github.com/danila_krut"),
+	Student.new(surname:"Прокопенко", name:"Юрий", patronymic:"Данилович", phone: "+78005553535"),
+	Student.new(surname: "Зубенко", name:"Михаил", patronymic: "Петрович", id:"32", phone:"+79528502711"),
 ]
 students.each do |student|
 	puts student, "\n"
