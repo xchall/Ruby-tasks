ac2a69a58c6901214d7ee0fe9d4238178dbe50fd
xchall <macsnebyvalov@yandex.ru>
2024-10-09 12:23:09 +0300

    Для проверки валидации

--- Students/main.rb
+++ Students/main.rb
@@ -1,9 +1,9 @@
 require_relative "student"
 students = [
-	Student.new(surname:"Таран", name:"Дмитрий", patronymic:"Сергеевич", email:"dimamolod@gmail.com"),
-	Student.new(surname:"Бачурин", name:"Данил", patronymic:"Петрович", id:"451", git:"github.com/danila_krut"),
-	Student.new(surname:"Прокопенко", name:"Юрий", patronymic:"Данилович", phone: "+78005553535"),
-	Student.new(surname: "Зубенко", name:"Михаил", patronymic: "Петрович", id:"32", phone:"+79528502711"),
+	Student.new(surname:"Таран", name:"Дмитрий", patronymic:"Сергеевич",phone:"+79528345611", email:"dimamolod@gmail.com", git:"https://github.com/dmitriy_proger"),
+	Student.new(surname:"Бачурин", name:"Данил", patronymic:"Петрович", id:"451", phone:"+79532503465", git:"github.com/danila_krut"),
+	Student.new(surname:"Прокопенко", name:"Юрий", patronymic:"Данилович", phone: "+78005553535", email:"Ura228_33@yandex.ru", teleg:"@Mishania"),
+	Student.new(surname: "Зубенко", name:"Михаил", patronymic: "Петрович", id:"32", phone:"+79528502711", teleg:"@dssxsx_Sd12D"),
 ]
 students.each do |student|
 	puts student, "\n"
