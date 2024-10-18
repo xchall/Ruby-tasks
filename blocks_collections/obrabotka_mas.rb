e54124302ae8e81beb6d6e126b08728cc7cef74f
xchall <macsnebyvalov@yandex.ru>
2024-10-18 16:34:54 +0300

    добавлен приватный сеттер

--- blocks_collections/obrabotka_mas.rb
+++ blocks_collections/obrabotka_mas.rb
@@ -1,12 +1,23 @@
 class ObrabotkaMas
+	private def mas=(mas)
+		if mas.is_a?(Array)
+			@mas = mas
+		else
+			raise ArgumentError, "Не является массивом #{mas}"
+		end
+	end
 	def initialize(mas)
-		@mas = mas
+		self.mas = mas
 	end
 	def get_elem_ind(index)
-		@mas[index]
+		if(index < @mas.length && index > 0)
+			@mas[index]
+		else 
+			"Out of range"
+		end
 	end
 	def array
- 		@array.dup
+ 		@array.dup #поверхностное копирование, для глубокого Marshal
  	end
 	def any?(&block)
 		if block_given?
@@ -21,3 +32,4 @@ class ObrabotkaMas
 		end
 	end
 end
+
