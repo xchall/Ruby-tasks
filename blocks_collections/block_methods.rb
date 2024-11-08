51641e22b2a31894596ea4f1617132f41a067c72
xchall <macsnebyvalov@yandex.ru>
2024-11-09 01:33:24 +0300

    Лаб3.Задание1.Добавили метод before_min_to_end
    разместить элементы, расположенные до минимального, в конце массива

--- blocks_collections/block_methods.rb
+++ blocks_collections/block_methods.rb
@@ -3,3 +3,8 @@ def count_after_max(mas)
 	lm_ind = mas.rindex(max)
 	mas.length - lm_ind - 1
 end
+def before_min_to_end(mas)
+	min = mas.min
+	m_ind = mas.index(min)
+	mas = mas[m_ind..mas.length-1] + mas[0..m_ind]
+end
