56ea1d4a10384d31d0e4b4881b44fa6a254d9852
xchall <macsnebyvalov@yandex.ru>
2024-11-22 13:04:27 +0300

    Лаб4.1 Применил паттерн Шаблон + рефакторинг

--- Students/classes_models/data_list.rb
+++ Students/classes_models/data_list.rb
@@ -3,18 +3,27 @@ class DataList
 	def initialize(elems:)
 		@elems = elems
 		@selected = [] #массив индексов выделенных элементов 
-		some_list = [] #массив заполненный определенным образом, реализация заполнения в наследнике
+		@some_list = [] #массив заполненный определенным образом, реализация заполнения в наследнике
 	end
 	def select(number)
 		if not (number <= @elems.size-1 && number >= 0)
 			raise Exception, "Неверный индекс"
-		if not (selected.include?(number)) selected.append(number)
+		end
+		if not (selected.include?(number)) 
+			selected.append(number)
+		end
+	end
 	def get_selected()
 		@selected 
+	end
 	def get_names()
-		raise Exception, "Реализация метода get_names в классе наследнике"
+		raise NoImplementedError, "Реализация метода get_names в классе наследнике"
 	end
 	def get_data()
-		DataTable(some_list)
+		fill_some_list()
+		DataTable.new(table:@some_list)
+	end
+	private def fill_some_list()
+		raise NoImplementedError, "Реализация операции заполнения some_list в классе наследнике"
 	end
 end
