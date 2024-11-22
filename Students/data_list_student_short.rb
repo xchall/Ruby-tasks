df51cfd4c9c875ae8ec8a30d79251c81dc70fa56
xchall <macsnebyvalov@yandex.ru>
2024-11-22 13:05:41 +0300

    Лаб4.1.7. Применил паттерн Шаблон

--- Students/data_list_student_short.rb
+++ Students/data_list_student_short.rb
@@ -1,17 +1,23 @@
-require_relative "C:/Users/max/Desktop/Ruby/Students/data_list"
+require_relative "C:/Users/max/Desktop/Ruby/Students/classes_models/data_list"
+require_relative "C:/Users/max/Desktop/Ruby/Students/classes_models/data_table"
 class DataListStudentShort < DataList
 	def initialize(elems:)
 		super(elems:elems)
+	end
 	def get_names()
 		["id", "surname_in", "git", "contact"]
 	end
-	def into_row(ind, el)
+	private def into_row(ind, el)
 		[ind, el.surname_in, el.git, el.contact] #все, кроме id
 	end
 	def get_data()
-		some_list = [] #отчищаем
-		@elems.each_with_index do |el, ind|
-			some_list.append(into_row(ind+1, el))#нумеруем начиная с 1, ind+1
+		fill_some_list()
 		super()
+	end
+	private def fill_some_list()
+		@some_list = [] #отчищаем
+		@elems.each_with_index do |el, ind|
+			@some_list.append(into_row(ind+1, el))#нумеруем начиная с 1, ind+1
+		end
 	end
 end
