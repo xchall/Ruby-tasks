56cb3d5c072f5d3e09f60a2938b90670ae0313f1
xchall <macsnebyvalov@yandex.ru>
2024-11-29 13:52:26 +0300

    Лаб4.1.2 добавил проверку индексов в get_element
    изменения в классе DataTable

--- Students/classes_models/data_table.rb
+++ Students/classes_models/data_table.rb
@@ -1,15 +1,19 @@
 class DataTable
-	private def table=(table:)
+	private def table=(table)
 		@table = table
 	end
 	def initialize(table:)
 		self.table = table
 	end
 	def get_element(n_row, n_col)
-		@table[n_row][n_col]
+		if (n_row >=0 && n_row <= row_count-1) && (n_col >=0 && n_col <= col_count-1)
+			@table[n_row][n_col]
+		else 
+			raise ArgumentError, "Неверный формат номера строки, или номера столбца"
+		end
 	end
 	def col_count
-		if @table.size == 0 ? 0 : @table[0].size
+		@table.size == 0 ? 0 : @table[0].size
 	end
 	def row_count
 		@table.size
