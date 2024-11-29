cfc65629d6f20fdf11f332d5480d76edfc79ecc9
xchall <macsnebyvalov@yandex.ru>
2024-11-29 14:11:02 +0300

    Лаб4.1.9 сеттер заменяющий массив объектов
    В DataList добавлен сеттер заменяющий массив объектов

--- Students/classes_models/data_list.rb
+++ Students/classes_models/data_list.rb
@@ -1,13 +1,17 @@
 require_relative "data_table"
 class DataList
-	def initialize(elems:)
+	#сеттер заменяющий массив объектов
+	def elems=(elems)
 		@elems = elems
+	end
+	def initialize(elems:)
+		self.elems = elems
 		@selected = [] #массив индексов выделенных элементов 
 		@some_list = [] #массив заполненный определенным образом, реализация заполнения в наследнике
 	end
 	def select(number)
 		if not (number <= @elems.size-1 && number >= 0)
-			raise Exception, "Неверный индекс"
+			raise ArgumentError, "Неверный индекс"
 		end
 		if not (selected.include?(number)) 
 			selected.append(number)
@@ -16,6 +20,7 @@ class DataList
 	def get_selected()
 		@selected 
 	end
+	#names - наименования столбцов у элементов 
 	def get_names()
 		raise NoImplementedError, "Реализация метода get_names в классе наследнике"
 	end
