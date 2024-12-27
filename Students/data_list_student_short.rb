a2f26b47daed7b124259d8973e9cd385374a241d
xchall <macsnebyvalov@yandex.ru>
2024-12-27 23:56:29 +0300

    Лаб4.1 Изменили реализацию шаблона
    Удален метод fill_some_list

--- Students/data_list_student_short.rb
+++ Students/data_list_student_short.rb
@@ -1,24 +1,15 @@
-require_relative "C:/Users/max/Desktop/Ruby/Students/classes_models/data_list"
-require_relative "C:/Users/max/Desktop/Ruby/Students/classes_models/data_table"
+require_relative "C:/Users/max/Desktop/3_curs_1_sem/Ruby/Students/classes_models/data_list"
+require_relative "C:/Users/max/Desktop/3_curs_1_sem/Ruby/Students/classes_models/data_table"
 class DataListStudentShort < DataList
 	def initialize(elems:)
 		super(elems:elems)
 		@names = ["number", "surname_in", "git", "contact"]
 	end
 	def get_names()#getter
-		@names
+		copy_names = @names.dup
+		copy_names
 	end
 	private def into_row(ind, el)
 		[ind, el.surname_in, el.git, el.contact] #все, кроме id
-	end
-	def get_data()
-		fill_some_list()
-		super()
-	end
-	private def fill_some_list()
-		@some_list = [] #отчищаем
-		@elems.each_with_index do |el, ind|
-			@some_list.append(into_row(ind+1, el))#нумеруем начиная с 1, ind+1
-		end
 	end
 end
